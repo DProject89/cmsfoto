34c6be3c5203acb574642288d0f62629d400c1b9
Meriyah Prasetyo <112935822+DProject89@users.noreply.github.com>
2022-09-15 11:26:28 +0700

    upload beberapa fitur baru

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-
+	// Fotografer
 	http.HandleFunc("/", fotografercontroller.Index)
 	http.HandleFunc("/fotografer", fotografercontroller.Index)
 	http.HandleFunc("/fotografer/index", fotografercontroller.Index)
@@ -15,5 +15,29 @@ func main() {
 	http.HandleFunc("/fotografer/edit", fotografercontroller.Edit)
 	http.HandleFunc("/fotografer/delete", fotografercontroller.Delete)
 
-	http.ListenAndServe(":8989", nil)
+	// Event
+	// http.HandleFunc("/", eventcontroller.Index)
+	// http.HandleFunc("/event", eventcontroller.Index)
+	// http.HandleFunc("/event/index", eventcontroller.Index)
+	// http.HandleFunc("/event/add", eventcontroller.Add)
+	// http.HandleFunc("/event/edit", eventcontroller.Edit)
+	// http.HandleFunc("/event/delete", eventcontroller.Delete)
+
+	// Event
+	// http.HandleFunc("/", eventgroupcontroller.Index)
+	// http.HandleFunc("/eventgroup", eventgroupcontroller.Index)
+	// http.HandleFunc("/eventgroup/index", eventgroupcontroller.Index)
+	// http.HandleFunc("/eventgroup/add", eventgroupcontroller.Add)
+	// http.HandleFunc("/eventgroup/edit", eventgroupcontroller.Edit)
+	// http.HandleFunc("/eventgroup/delete", eventgroupcontroller.Delete)
+
+	// Event
+	// http.HandleFunc("/", evenlistcontroller.Index)
+	// http.HandleFunc("/evenlist", evenlistcontroller.Index)
+	// http.HandleFunc("/evenlist/index", evenlistcontroller.Index)
+	// http.HandleFunc("/evenlist/add", evenlistcontroller.Add)
+	// http.HandleFunc("/evenlist/edit", evenlistcontroller.Edit)
+	// http.HandleFunc("/evenlist/delete", evenlistcontroller.Delete)
+
+	http.ListenAndServe(":8080", nil)
 }
